03ab08553733d84297e3342e65a40033413e7bcb
PMDEV <patriciamagodev@gmail.com>
2025-02-08 20:20:14 -0400

    Agregando Botón de Modo DarkMode

---
+++ personalPortfolio/script.js
@@ -0,0 +1,12 @@
+const toggleButton = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+}
+
+toggleButton.addEventListener('click', () => {
+    const isDark = document.documentElement.getAttribute('data-theme') === 'dark';
+    document.documentElement.setAttribute('data-theme', isDark ? 'light' : 'dark');
+    localStorage.setItem('theme', isDark ? 'light' : 'dark');
+});
